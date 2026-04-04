bef72068eafc661938f8840e6349d64e5e87bc42
tabassumishadneha <tabassumesadneha@gmail.com>
2026-04-04 21:12:28 +0600

    Add Blog component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import { Contact } from "@/components/Contact";
 import Projects from "@/components/Projects";
+import Blog from "@/components/Blog";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Projects />
+      <Blog />
       <Contact />
     </main>
   );
